8d8cf1a4f2a6fdab45d0ac32020b39f7f40f0f59
mustii <mustii@mmk.one>
2018-06-03 20:08:21 +0300

    Small Fix

--- newsApp/AppDelegate.swift
+++ newsApp/AppDelegate.swift
@@ -15,11 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        // Override point for customization after application launch.
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         let root = MainVC(collectionViewLayout: UICollectionViewFlowLayout())
         window?.rootViewController = UINavigationController(rootViewController: root)
         window?.makeKeyAndVisible()
-        // Override point for customization after application launch.
+        
         return true
     }
 
