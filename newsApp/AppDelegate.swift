185eb64cfc5a8952d6f66a3726056aa96b4d045f
mustii <mustii@mmk.one>
2018-06-03 15:52:52 +0300

    Initial Commit

--- newsApp/AppDelegate.swift
+++ newsApp/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let root = MainVC(collectionViewLayout: UICollectionViewLayout())
+        window?.rootViewController = UINavigationController(rootViewController: root)
+        window?.makeKeyAndVisible()
         // Override point for customization after application launch.
         return true
     }
---
+++ newsApp/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  newsApp
+//
+//  Created by Mustafa Khalil on 6/3/18.
+//  Copyright © 2018 Mustafa Khalil. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension MainVC {
+    //MARK:- Collection View function
+}
+
